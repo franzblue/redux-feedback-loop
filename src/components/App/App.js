8c6c95b661fecec057506b1b4424ff8ca01c5352
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-31 16:09:36 -0500

    app.js routing to components, also testing nextPage button in Feeling Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Feeling from '../Feeling/Feeling';
 import Understanding from '../Understanding/Understanding';
@@ -10,20 +12,23 @@ import Review from '../Review/Review';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-        <Feeling />
-        <Understanding />
-        <Support />
-        <Comments />
-        <Review />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br/>
+        </div>
+
+        <Route exact path="/" component={Feeling}/>
+        <Route exact path="/2" component={Understanding}/>
+        <Route exact path="/3" component={Support}/>
+        <Route exact path="/4" component={Comments}/>
+        <Route exact path="/5" component={Review}/>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect()(App);
--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Feeling extends Component {
+
+    nextPage = () => {
+        console.log('check');
+        // this will happen this weekend
+        this.props.history.push('/2');
+    }
+
     render() {
         return(
+            <>
             <h2>How are you feeling today?</h2>
+            <button onClick={this.nextPage}>Next</button>
+            </>
         )
     }
 }
